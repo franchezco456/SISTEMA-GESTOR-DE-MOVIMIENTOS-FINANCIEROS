4c58a5830d04b95990a53ef57e3d444fea6f909a
jesulini <jesusd06@hotmail.com>
2025-05-10 10:57:20 -0500

    correccion de ReglasFinancieras

--- Model/Entidades/ReglasFinancieras.php
+++ Model/Entidades/ReglasFinancieras.php
@@ -53,25 +53,4 @@ public function setCategoria($categoria){
 }
 
 }
-
-$a = new ReglasFinancieras(109802, 5551, 300000, "fechapapu", "comida");
-echo("<br> id regla : ".$a->getIdReglaFinanciera());
-echo("<br> id cuenta :".$a->getIdCuenta());
-echo("<br> valor regla :".$a->getValor_Regla());
-echo("<br> fecha creacion :".$a->getFechaCreacion());
-echo("<br> categoria :".$a->getCategoria());
-
-$a->setIdReglaFinanciera(1043649);
-$a->setIdCuenta(4444);
-$a->setValor_Regla(200000);
-$a->setFechaCreacion("fechaxD");
-$a->setCategoria("enserio comida");
-
-echo("<br> nuevo id regla : ".$a->getIdReglaFinanciera());
-echo("<br> nuevo cuenta :".$a->getIdCuenta());
-echo("<br> nuevo valor regla :".$a->getValor_Regla());
-echo("<br> nuevo fecha creacion :".$a->getFechaCreacion());
-echo("<br> nuevo categoria :".$a->getCategoria());
-
-
 ?>
