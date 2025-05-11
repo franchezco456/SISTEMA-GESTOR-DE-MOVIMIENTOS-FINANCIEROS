2e568319e4935f89e15017981984aa95fb4cbf8c
jesulini <jesusd06@hotmail.com>
2025-05-11 00:47:59 -0500

    añadir el selector de categoria en ModificarCuenta

--- View/ModificarCuenta.php
+++ View/ModificarCuenta.php
@@ -23,6 +23,18 @@
             <span class="change-text">Modificar tope máximo</span>
             <input type="number" placeholder="Nuevo tope máximo">
 
+            <label for="categoria">Seleccionar categoría financiera:</label>
+<select id="categoria" name="categoria" required>
+    <option value="">Selecciona una categoría</option>
+    <option value="alimentacion">Alimentación</option>
+    <option value="transporte">Transporte</option>
+    <option value="entretenimiento">Entretenimiento</option>
+    <option value="salud">Salud</option>
+    <option value="educacion">Educación</option>
+    <option value="ahorro">Ahorro</option>
+    <option value="otros">Otros</option>
+</select>
+
             <button type="submit" class="save-btn">Guardar Cambios</button>
         </form>
     </div>
